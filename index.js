745da58bb88abfe026188656f5e692cdcf3ec7dd
Shankar_siva <88714813+SIMPLE-TECNIQUES@users.noreply.github.com>
2022-10-04 15:29:19 +1000

    Add files via upload
    Made a notification page with simple HTML , CSS ,  JAVA_Script
    frontend challenge

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("input").checked=true;
+document.getElementById("mark").addEventListener('click',notified);
+function notified()
+{document.getElementById("bt_num").innerHTML="0";
+// document.getElementById("chk").checked=false;
+}
+document.querySelectorAll("input").checked=true;
